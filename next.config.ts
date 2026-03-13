e142bc28fcdec2aa9a13af0dec5feb5bdee05557
Khetsopon11 <khetsoponkrongyut@gmail.com>
2026-03-13 15:25:26 +0700

    Update GitHub Actions permissions and change Next.js basePath for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-   basePath: "/cicd",
+   basePath: "/CI-CD-Workshop",
 };
 
 export default nextConfig;
